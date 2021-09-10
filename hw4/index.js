0716f86cd3f7334a25a9ef590616d3674d54b9c0
Anastasiya Piatrova3 <anastasiya_piatrova3@epam.com>
2021-09-10 11:09:12 +0200

    add one more Pizza.js for learning

--- hw4/index.js
+++ hw4/index.js
@@ -2,12 +2,19 @@ const Card = require('./card');
 const DebitCard = require('./card');
 const CreditCard = require('./card');
 const PizzaPapa = require('./PizzaPapa').default;
+const Pizza = require('./Pizza');
 
 console.log(papaBo = new PizzaPapa('', ['cheese']));
 console.log(papaJoe = new PizzaPapa('small'));
 console.log(papaBo.getPrice());
 console.log(papaBo.getCallories());
 console.log(papaJoe.addToppings());
+
+console.log(myPizza = new Pizza(Pizza.SIZES.small));
+console.log(myPizza.addToppings(Pizza.TOPPINGS.oregano));
+console.log(myPizza.isVegan);
+console.log(myPizza.removeTopping(Pizza.TOPPINGS.salami));
+console.log(myPizza.addToppings(Pizza.TOPPINGS.cheese));
 
 console.log(masterCard = new Card());
 console.log(masterCard._getCardBalance());
